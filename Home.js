f64e7eddbe8d039563ab0d6bfe086af032e23457
fanfairo <153469014+fanfairo@users.noreply.github.com>
2024-03-24 11:28:54 -0400

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>This is my first attempt at using react in this manner.
+        I do hope you enjoy it. I know I have enjoyed making it. Thank
+        you for your time.
+        </p>
+ 
+      
+      </div>
+    );
+  }
+}
+ 
+export default Home
